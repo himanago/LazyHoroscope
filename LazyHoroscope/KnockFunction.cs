035984a96736b54346dd6f90119924bb85beaddd
T.Hirabayashi <baa_ram_ewe_shipping@live.jp>
2018-12-20 17:11:32 +0900

    初期コミット

---
+++ LazyHoroscope/KnockFunction.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace LazyHoroscope
+{
+    public static class KnockFunction
+    {
+        [FunctionName("KnockFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return (ActionResult)new OkObjectResult("‚¨‚Í‚æ‚¤");
+        }
+    }
+}
