ea285104a858d3e93e585b0a62ce68f463b1bda3
pca02448 <pca02448@outlook.jp>
2019-02-22 16:21:15 +0900

    Alexa対応＋Clovaと処理を共通化するためリファクタリング

---
+++ LazyHoroscope/ClovaFunction.cs
@@ -0,0 +1,51 @@
+﻿using CEK.CSharp;
+using CEK.CSharp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace LazyHoroscope
+{
+    public static class ClovaFunction
+    {
+        [FunctionName("ClovaFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            CEKResponse response = null;;
+
+            if (req.Headers.TryGetValue("SignatureCEK", out var signature))
+            {
+                var client = new ClovaClient();
+                var request = await client.GetRequest(signature, req.Body);
+
+                // IntentRequestのみここにくる
+                if (request.Request.Type == RequestType.IntentRequest)
+                {
+                    // メイン処理
+                    var result = IntentHandler.Handle(request.Request.Intent.Name);
+                    response = result.GetClovaResponse();
+                }
+                else
+                {
+                    // エラー
+                    response = new CEKResponse();
+                    response.AddText("よくわかりませんでしたが、たぶんラッキーな1日になると思いますよ。");
+                    response.ShouldEndSession = true;
+                }
+            }
+            else
+            {
+                // エラー
+                response = new CEKResponse();
+                response.AddText("よくわかりませんでしたが、きっと大丈夫ですよ。");
+                response.ShouldEndSession = true;
+            }
+            return new OkObjectResult(response);
+        }
+    }
+}
---
+++ LazyHoroscope/IntentHandler.cs
@@ -0,0 +1,48 @@
+﻿using LazyHoroscope.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyHoroscope
+{
+    public static class IntentHandler
+    {
+        public static IntentResult Handle(string intentName)
+        {
+            var result = new IntentResult();
+
+            switch (intentName)
+            {
+                case "ZodiacIntent":
+                    // 占い結果を返す。今回は適当占いなのでランダムにどちらか
+                    // (本来はここでスロットの星座を受け取ってそれに基づいた処理を行う)
+                    if (new System.Random().Next() % 2 == 0)
+                    {
+                        result.AddMessage("今日は絶好調！とてもよい1日になりますよ！");
+                    }
+                    else
+                    {
+                        result.AddMessage("今日はあまりついてないかも。転ばないように気を付けてくださいね。");
+                    }
+                    result.ShouldEndSession = true;
+                    break;
+
+                case "AMAZON.HelpIntent":
+                case "Clova.GuideIntent":
+                    // 使い方
+                    result.AddMessage("あなたの星座を教えてください。占ってあげます。");
+                    result.ShouldEndSession = false;
+                    break;
+
+                default:
+                    // その他のインテントの場合
+                    result.AddMessage("よくわかりませんでしたが、まあまあだと思います。");
+                    result.ShouldEndSession = false;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
